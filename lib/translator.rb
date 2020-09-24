7f348a301091d09461f204cb1afdfef559b40d98
Sean Delaney <seanmartindelaney@gmail.com>
2020-09-24 05:35:18 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -17,6 +17,7 @@ def get_japanese_emoticon(file_to_load, emoticon)
   #binding.pry 
   hash.each do |emoji, language|
     if language[:english] == emoticon
+      emoticon = language[:japanese]
       binding.pry 
     end
   end
