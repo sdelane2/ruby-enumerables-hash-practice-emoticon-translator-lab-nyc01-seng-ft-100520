3cb9cca5b72db779a8cdefaf2a14956f0d0379f9
Sean Delaney <seanmartindelaney@gmail.com>
2020-09-24 05:43:06 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -16,8 +16,7 @@ def get_japanese_emoticon(file_to_load, emoticon)
   hash = load_library(file_to_load)
   #binding.pry 
   hash[:english].each do |a, b|
-    binding.pry
-  end
+  binding.pry
 end
 
 def get_english_meaning
