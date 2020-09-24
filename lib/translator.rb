59fd63c872520fb05a3c53f0e989f28b41daacd7
Sean Delaney <seanmartindelaney@gmail.com>
2020-09-24 04:53:27 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -19,7 +19,7 @@ def get_japanese_emoticon(file_to_load, emoticon)
      if language[:english] == emoticon 
         return emoticon = language[:japanese]
       else
-        return "Sorry, that emoticon was not found"
+         puts "Sorry, that emoticon was not found"
     end
   end
 end
