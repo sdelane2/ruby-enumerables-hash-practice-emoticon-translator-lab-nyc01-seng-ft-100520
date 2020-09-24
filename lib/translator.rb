9229229388b53ee2720fef846542dd4abd8d77a1
Sean Delaney <seanmartindelaney@gmail.com>
2020-09-24 04:33:31 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -1,12 +1,37 @@
 # require modules here
+require 'yaml'
+require 'pry'
 
-def load_library
-  # code goes here
+def load_library(file_to_load)
+  new_hash = {}
+  YAML.load_file(file_to_load).each do |key, value|
+    if !new_hash[key]
+      new_hash[key] = {:english => value[0], :japanese => value[1]}
+    end
+  end
+  new_hash
 end
 
-def get_japanese_emoticon
-  # code goes here
+def get_japanese_emoticon(file_to_load, emoticon)
+  hash = load_library(file_to_load)
+  #binding.pry 
+  hash.detect do |emoji, language|
+     if language[:english] == emoticon 
+      emoticon = language[:japanese]
+      return emoticon unless 
+    end 
+  end
+  new_array = []
+  hash.collect do |emoji, language|
+    new_array << language.to_a
+  end
+  new_array2 = []
+  new_array.each do |language, value|
+    new_array2 << value[1]
+  end
+  
 end
+
 
 def get_english_meaning
   # code goes here
